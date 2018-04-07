188b9e2617c620df67ab0f2818c5c464f1b2de92
guan <1297089986@qq.com>
2018-04-07 21:28:39 +0800

    k

--- frontend/components/ShopCart.php
+++ frontend/components/ShopCart.php
@@ -68,7 +68,7 @@ class ShopCart extends Component
         $userId=\Yii::$app->user->id;
         foreach ($this->cart as $goodsId=>$num){
             //判断当前商户存不存在
-            $cartDb=Cart::findOne(['goods_id'=>$goodsId,'user_id'=>$userId]);
+            $cartDb=Cart::findOne(['goods_id'=>$goodsId,'member_id'=>$userId]);
             //判断
             if ($cartDb){
                 //+ 修改操作
